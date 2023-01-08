3ea4d0f3ffcd93388144438bf192fee025696c6e
Chatmalow <maxime.pie.mail@gmail.com>
2023-01-08 09:08:22 +0100

    feat: add bonus page

--- src/App.tsx
+++ src/App.tsx
@@ -18,20 +18,47 @@ type Member = {
   level: number;
   cost: number;
 }
+
+type Effect = {
+  target: string;
+  multiplier: number;
+}
+
+type Bonus = {
+  name: string;
+  cost: number;
+  effect: Effect;
+  isBought: boolean;
+  isAvailable: boolean;
+}
 type GameType = {
   score: number,
   setScore: (score: number) => void
   team: Member[],
   hire: (memberName: string) => void,
   scorePerSecond: number,
+  bonusList: Bonus[],
+  acquireBonus: (bonus: Bonus) => void
 }
 
 function GameProvider(props: any) {
   const [score, setScore] = useState(0);
   const [team, setTeam] = useState([
-    { name: "André", age: 2, level: 0, cost: 10, scorePerSecond: 0.1 },
-    { name: "Marine", age: 20, level: 0, cost: 100, scorePerSecond: 1},
-    { name: "Marie Curie", age: 220, level: 0, cost: 100000000, scorePerSecond: 100000 },
+    {name: "André", age: 2, level: 0, cost: 10, scorePerSecond: 0.1},
+    {name: "Marine", age: 20, level: 0, cost: 100, scorePerSecond: 1},
+    {name: "Marie Curie", age: 220, level: 0, cost: 100000000, scorePerSecond: 100000},
+  ]);
+
+  const [bonusList, setBonusList] = useState([
+    {
+      name: "Canard en plastique", cost: 10, effect: {
+        target: "André",
+        multiplier: 2,
+      },
+      isBought: false,
+      isAvailable: true,
+    },
+    {name: "Bureau marcheur", cost: 100, effect: {target: "Marine", multiplier: 2}, isAvailable: true, isBought: false},
   ]);
 
   const [scorePerSecond, setScorePerSecond] = useState(0.1);
@@ -44,11 +71,39 @@ function GameProvider(props: any) {
     return () => clearInterval(interval);
   }, [scorePerSecond]);
 
+  useEffect(() => {
+    // Recalculate the scorePerSecond based on the team and the bonuses
+    let newScorePerSecond = 0;
+    team.forEach(member => {
+      const bonus = bonusList.find(bonus => bonus.effect.target === member.name);
+      newScorePerSecond += member.scorePerSecond * member.level * (bonus?.isBought ? bonus.effect.multiplier : 1);
+    });
+
+    setScorePerSecond(newScorePerSecond);
+  }, [team, bonusList]);
+
   return (
-    <GameContext.Provider value={{ score, setScore, hire, team, scorePerSecond }}>
+    <GameContext.Provider value={{score, setScore, hire, team, scorePerSecond, bonusList, acquireBonus}}>
       {props.children}
     </GameContext.Provider>
   );
+
+  /**
+   * Acquire the bonus with the given name
+   * Apply the effect of the bonus
+   * @param bonus
+   */
+  function acquireBonus(bonus: Bonus) {
+    // Update the bonus list
+    setBonusList(bonusList => {
+      return [...bonusList].map(b => {
+        if (b.name === bonus.name) {
+          return {...b, isBought: true, isAvailable: false}
+        }
+        return b;
+      });
+    });
+  }
 
   /**
    * Add one level to the member
@@ -77,22 +132,22 @@ function GameProvider(props: any) {
 function Team() {
 
   // get team from context
-  const { team, hire, score } = useContext(GameContext);
+  const {team, hire, score} = useContext(GameContext);
 
 
-  return(
+  return (
     <div className="Team">
       <h3>Équipe</h3>
-        {team.map((member, index) => (
-          <div key={index}>
-            <p>
-              <span>{member.level} </span>
-              {member.name} ({member.age} ans)
-              <span> {member.cost} kn</span>
-              <button disabled={score < member.cost} onClick={() => hire(member.name)}>Recruter</button>
-            </p>
-          </div>
-        ))}
+      {team.map((member, index) => (
+        <div key={index}>
+          <p>
+            <span>{member.level} </span>
+            {member.name} ({member.age} ans)
+            <span> {member.cost} kn</span>
+            <button disabled={score < member.cost} onClick={() => hire(member.name)}>Recruter</button>
+          </p>
+        </div>
+      ))}
     </div>
   )
 
@@ -101,7 +156,7 @@ function Team() {
 function Counter() {
 
   // get score from context
-  const { score, setScore, scorePerSecond} = useContext(GameContext);
+  const {score, setScore, scorePerSecond} = useContext(GameContext);
 
   // Rounded to 2 decimals
   const formattedScore = Math.round(score * 100) / 100;
@@ -119,22 +174,64 @@ function Counter() {
       <button onClick={() => setScore(score + 1)}>
         +1
       </button>
+      <button onClick={() => setScore(score + 10)}>
+        +10
+      </button>
+      <button onClick={() => setScore(score + 100000)}>
+        +100000
+      </button>
+    </div>
+  )
+}
+
+function BonusList() {
+
+  // get bonusList from context
+  const {bonusList, acquireBonus, score} = useContext(GameContext);
+
+  return (
+    <div className="BonusList">
+      <h3>Bonus</h3>
+      {bonusList.filter(({isAvailable}) => isAvailable).map((bonus, index) => (
+        <div key={index} className="Bonus">
+          <p>
+            {bonus.name} ({bonus.cost} kn)
+          </p>
+          <button disabled={score < bonus.cost} onClick={() => acquireBonus(bonus)}>Acheter</button>
+        </div>
+      ))}
     </div>
   )
 }
 
 function Game() {
-    return (
-      <GameProvider>
-        <div className="Game">
-          <Counter/>
-          <div className="Upgrades">
-            <h2>Améliorations</h2>
-            <Team/>
+  const [selectedPanel, setSelectedPanel] = useState<"team" | "bonus">("team");
+  return (
+    <GameProvider>
+      <div className="Game">
+        <Counter/>
+        <div className="Upgrades">
+          <h2>Améliorations</h2>
+          <div className="Upgrades__tabs">
+            <button
+              onClick={() => setSelectedPanel("team")}
+              className={selectedPanel === "team" ? "selected" : ""}
+            >
+              Équipe
+            </button>
+            <button
+              onClick={() => setSelectedPanel("bonus")}
+              className={selectedPanel === "bonus" ? "selected" : ""}
+            >
+              Bonus
+            </button>
           </div>
+          {selectedPanel === "team" && <Team/>}
+          {selectedPanel === "bonus" && <BonusList/>}
         </div>
-      </GameProvider>
-    )
+      </div>
+    </GameProvider>
+  )
 }
 
 function App() {
